d6de03265eb81c16fba6035a1a07fe767737345e
teruzuki <yuj19@pitt.edu>
2016-10-13 23:04:31 -0400

    V2
    Added JS fuction at field guide page
    Added jQuery fadeIn() fuction at index page
    Added a few lines of Bio on Author page
    Added search box (not functional though)

--- js/indexAction.js
+++ js/indexAction.js
@@ -6,6 +6,7 @@ function imageEnlarge(small, large) {
         document.getElementById('largeImg').src = large;
     }, 1)
 }
+
 function showPanel() {
 	document.getElementById('enlargedPanel').style.display = 'block';
 }
@@ -16,3 +17,37 @@ function hideAfterClick() {
     if(window.getSelection)
         window.getSelection().removeAllRanges();
 }
+
+function submissionValidation() {
+	var firstName = document.getElementById('firstname').value;
+	var lastName = document.getElementById('lastname').value;
+	var location = document.getElementById('Location').value;
+
+	if (firstName == "")
+    {
+    	window.alert("please put in a first name!");
+    	
+    }
+    else if (lastName == "")
+    {
+    	window.alert("Please put in a last name!");
+    	
+    }
+    else if (location == "")
+    {
+    	window.alert("Please put in a location!");
+    	
+    }
+    else 
+    {
+    	submissionAction();
+    }
+}
+
+function submissionAction() {
+	var firstName = document.getElementById('firstname').value;
+	var lastName = document.getElementById('lastname').value;
+	var location = document.getElementById('Location').value;
+	alert(firstName + " " + lastName + ", you have successfully submitted your picture you shoot at " + location + "! \nThank you very much!");
+}
+
